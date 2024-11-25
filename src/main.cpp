dd797ff8f33f8c185fa758376efe630a56546d40
Roy Chung <roychungz99@gmail.com>
2024-11-25 01:10:18 -0500

    load background

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include "bn_core.h"
+#include "bn_regular_bg_ptr.h"
+
+#include "bn_regular_bg_items_background.h"
+
 
 int main()
 {
     bn::core::init();
+
+    //load background
+    bn::regular_bg_ptr background = bn::regular_bg_items::background.create_bg(0, 0);
 
     while(true)
     {
