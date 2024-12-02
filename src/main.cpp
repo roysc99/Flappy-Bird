96af79a54cb6e536fdaffc8aa7a95dc9716c3ea8
Roy Chung <roychungz99@gmail.com>
2024-12-02 02:44:15 -0500

    bird, tube, floor sprites

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include "bn_core.h"
 #include "bn_regular_bg_ptr.h"
+#include "bn_sprite_ptr.h"
 
 #include "bn_regular_bg_items_background.h"
+#include "bn_sprite_items_flappybird.h"
+#include "bn_sprite_items_tuberoot.h"
+#include "bn_sprite_items_tuberoot.h"
 
 
 int main()
@@ -10,6 +14,12 @@ int main()
 
     //load background
     bn::regular_bg_ptr background = bn::regular_bg_items::background.create_bg(0, 0);
+
+    //load bird sprite
+    bn::sprite_ptr flappybird = bn::sprite_items::flappybird.create_sprite(0,0);
+
+    //load bird sprite
+    bn::sprite_ptr tube = bn::sprite_items::tuberoot.create_sprite(10,0);
 
     while(true)
     {
