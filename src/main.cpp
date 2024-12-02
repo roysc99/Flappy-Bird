7d37eac41148f63d6ae93c95e0759cdd285f1c98
Roy Chung <roychungz99@gmail.com>
2024-12-02 13:21:38 -0500

    add flying, gravity, and text

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "bn_core.h"
 #include "bn_regular_bg_ptr.h"
 #include "bn_sprite_ptr.h"
+#include "bn_keypad.h"
+#include "bn_sprite_text_generator.h"
+#include "bn_vector.h"
+#include "common_info.h"
+#include "common_variable_8x8_sprite_font.h"
 
 #include "bn_regular_bg_items_background.h"
 #include "bn_sprite_items_flappybird.h"
@@ -18,11 +23,32 @@ int main()
     //load bird sprite
     bn::sprite_ptr flappybird = bn::sprite_items::flappybird.create_sprite(0,0);
 
-    //load bird sprite
-    bn::sprite_ptr tube = bn::sprite_items::tuberoot.create_sprite(10,0);
+    //object to generate text
+    bn::sprite_text_generator text_generator(common::variable_8x8_sprite_font);
+    bn::vector<bn::sprite_ptr, 32> text_sprites;
+    //printing default text
+    text_generator.generate(-60, -68, "(Press up key to start)", text_sprites);
 
-    while(true)
+   bool game_started = false; //flag to check if the game has started
+
+    while (true)
     {
+        if (bn::keypad::up_pressed())
+        {
+            if (!game_started)
+            {
+                // Clear the text sprites when the game starts
+                text_sprites.clear();
+                game_started = true; // Mark the game as started
+            }
+
+            flappybird.set_y(flappybird.y() - 20); // Move the bird up
+        }
+        else if (game_started)
+        {
+            flappybird.set_y(flappybird.y() + 1); // Apply gravity only if the game has started
+        }
+
         bn::core::update();
     }
 }
